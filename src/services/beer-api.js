31697db607843f0f37c708e4cadbf11b9d157beb
Harumi Terayama <hterayama@gmail.com>
2021-03-18 20:33:49 +0100

    iteration 3 completed

---
+++ src/services/beer-api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const listBeers = async () => {
+  const response = await axios.get('https://ih-beers-api2.herokuapp.com/beers');
+  console.log(response);
+  return response.data;
+};
--- src/views/Beers.jsx
+++ src/views/Beers.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import Header from './../components/Header';
+import { listBeers } from './../services/beer-api';
 
 class Beers extends Component {
   state = {
     beers: [],
   };
+
+  componentDidMount() {
+    this.loadBeerList();
+  }
+
+  async loadBeerList() {
+    const beers = await listBeers();
+    this.setState({
+      beers: beers,
+    });
+  }
   render() {
     return (
       <div>
         <Header></Header>
         <h1>List of Beers</h1>
+        <div className="list-body">
+          {this.state.beers.map((beer) => (
+            <div key={beer.id} className="beer-item">
+              <div>
+                <img
+                  className="beer-img"
+                  src={beer.image_url}
+                  alt={beer.name}
+                />
+              </div>
+              <div className="beer-description">
+                <h3>{beer.name}</h3>
+                <p className="beer-tagline">{beer.tagline}</p>
+                <p className="beer-created-by">
+                  Created by: {beer.contributed_by}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
